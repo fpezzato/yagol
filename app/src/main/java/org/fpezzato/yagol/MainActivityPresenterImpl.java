cddaaf6c67005b332cb293880770a6dd0d04bc6f
francesco pezzato <fpezzato@gmail.com>
2015-06-18 21:40:15 +0100

    WIP - writing main engine

--- app/src/main/java/org/fpezzato/yagol/MainActivityPresenterImpl.java
+++ app/src/main/java/org/fpezzato/yagol/MainActivityPresenterImpl.java
@@ -1,12 +1,18 @@
 package org.fpezzato.yagol;
 
+import android.os.Handler;
+
 import com.google.common.collect.ArrayTable;
 import com.google.common.collect.ContiguousSet;
 import com.google.common.collect.DiscreteDomain;
 import com.google.common.collect.Range;
 
+import org.fpezzato.yagol.biz.GolEngine;
 import org.fpezzato.yagol.mvp.BaseMvpPresenter;
 import org.fpezzato.yagol.mvp.MvpState;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by francesco on 17/06/2015.
@@ -15,26 +21,42 @@ public class MainActivityPresenterImpl extends BaseMvpPresenter<MainActivityView
 
 	public static final String KEY_MATRIX = MainActivityPresenterImpl.class.getCanonicalName() + ";KEY_MATRIX";
 
-	ArrayTable<Integer, Integer, Boolean> mMatrix;
+	private ArrayTable<Integer, Integer, Boolean> mMatrix;
+
+	private GolEngine mGolEngine;
+
+	private static final int INTERVAL = 100; //100 millisec clock tick.
+
+	final Handler handler = new Handler();
+	Timer timer = new Timer(false);
+	TimerTask timerTask = new TimerTask() {
+		@Override
+		public void run() {
+			handler.post(new Runnable() {
+				@Override
+				public void run() {
+				}
+			});
+		}
+	};
 
 	@Override
 	public void initialize(MainActivityView view, MvpState state) {
 		super.initialize(view, state);
+		mGolEngine = new GolEngine();
 		if (state.containsKey(KEY_MATRIX)) {
 			//TODO: not the pourpose of this app. Working with serializable to save dev-time.
 			mMatrix = (ArrayTable<Integer, Integer, Boolean>) state.getSerializable(KEY_MATRIX);
 		} else {
-
 			//FIXME - just demo code
 			Range<Integer> rows = Range.closed(0, 1000);
 			Range<Integer> columns = Range.closed(0, 1000);
 			mMatrix = ArrayTable.create(ContiguousSet.create(rows, DiscreteDomain.integers()), ContiguousSet.create(columns, DiscreteDomain.integers()));
 
-			for(int i = 0; i < 90;i++){
-				mMatrix.put(i,i,true);
+			for (int i = 0; i < 90; i++) {
+				mMatrix.put(i, i, true);
 			}
 		}
-
 	}
 
 	@Override
@@ -46,12 +68,16 @@ public class MainActivityPresenterImpl extends BaseMvpPresenter<MainActivityView
 
 	@Override
 	public void start() {
-
 		getMvpView().drawMatrix(mMatrix);
+
+		timer.schedule(timerTask, INTERVAL);
 	}
 
 	@Override
 	public void stop() {
 
 	}
+
+
+
 }
--- app/src/main/java/org/fpezzato/yagol/biz/GolEngine.java
+++ app/src/main/java/org/fpezzato/yagol/biz/GolEngine.java
@@ -1,7 +1,12 @@
 package org.fpezzato.yagol.biz;
 
 
-import com.google.common.collect.Table;
+import android.support.annotation.VisibleForTesting;
+
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ArrayTable;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Table.Cell;
 
 import static com.google.common.base.Optional.fromNullable;
@@ -11,25 +16,6 @@ import static com.google.common.base.Optional.fromNullable;
  */
 public class GolEngine {
 
-
-	//public GolEngine( ) {
-		//mMatrix = create(ContiguousSet.create(Range.closed(0, rows), integers()), ContiguousSet.create(Range.closed(0, columns), integers()));
-	//}
-
-	//public void setAliveCell(int row, int column) {
-		//mMatrix.put(row, column, true);
-	//}
-
-	/*
-
-	{
-		Range<Integer> rows = Range.closed(0, 100);
-		Range<Integer> columns = Range.closed(0, 100);
-
-		mMatrix = ArrayTable.create(create(rows, integers()), create(columns, integers()));
-	}
-*/
-
 	/**
 	 * Any live cell with fewer than two live neighbours dies, as if caused by under-population.
 	 * Any live cell with two or three live neighbours lives on to the next generation.
@@ -37,28 +23,63 @@ public class GolEngine {
 	 * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 	 */
 
+	ArrayTable<Integer, Integer, Boolean> mSwapTable;
+
+	public ArrayTable<Integer, Integer, Boolean> computeGeneration(final ArrayTable<Integer, Integer, Boolean> currentState) {
+		Preconditions.checkNotNull(currentState);
+		if (mSwapTable == null) {
+			mSwapTable = ArrayTable.create(currentState);
+		}
+		FluentIterable.from(currentState.cellSet()).transform(new Function<Cell<Integer, Integer, Boolean>, Void>() {
+			@Override
+			public Void apply(Cell<Integer, Integer, Boolean> input) {
+				int neighboursCount = computeNeighboursCount(currentState, input);
+				boolean isAlive = input.getValue() != null ? input.getValue() : false;
+				boolean alive = computeRule1(isAlive, neighboursCount)
+					&& computeRule2(isAlive, neighboursCount)
+					&& computeRule3(isAlive, neighboursCount)
+					&& computeRule4(isAlive, neighboursCount);
+				mSwapTable.put(input.getRowKey(), input.getColumnKey(), alive);
+				return null;
+			}
+		}).size();
+
+		return mSwapTable;
+
+	}
+
+
+	@VisibleForTesting
 	public boolean computeRule1(boolean isAlive, int neighboursCount) {
 		return isAlive && !(neighboursCount < 2);
 	}
 
+
+	@VisibleForTesting
 	public boolean computeRule2(boolean isAlive, int neighboursCount) {
 		return isAlive && (neighboursCount == 2 || neighboursCount == 3);
 	}
 
+
+	@VisibleForTesting
 	public boolean computeRule3(boolean isAlive, int neighboursCount) {
 		return isAlive && !(neighboursCount > 3);
 	}
 
+
+	@VisibleForTesting
 	public boolean computeRule4(boolean isAlive, int neighboursCount) {
 		return !isAlive && neighboursCount == 3;
 	}
 
-	public int computeNeighboursCount(Table<Integer, Integer, Boolean> matrix, Cell<Integer, Integer, Boolean> cell) {
+
+	@VisibleForTesting
+	public int computeNeighboursCount(ArrayTable<Integer, Integer, Boolean> matrix, Cell<Integer, Integer, Boolean> cell) {
 		int count = 0;
 		for (int i = cell.getRowKey() - 1; i <= cell.getRowKey() + 1; i++) {
 			for (int j = cell.getColumnKey() - 1; j <= cell.getColumnKey() + 1; j++) {
 				if (!(i < 0 || j < 0)) {
-					count += fromNullable(matrix.get(i, j)).or(false)
+					count += fromNullable(matrix.at(i, j)).or(false)
 						? 1 : 0;
 				}
 			}
