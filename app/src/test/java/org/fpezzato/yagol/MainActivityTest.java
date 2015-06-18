10b9a0c29aa0b0a6ed1852e3238716252210bdff
francesco pezzato <fpezzato@gmail.com>
2015-06-18 20:17:47 +0100

    Code clean up.

--- app/src/test/java/org/fpezzato/yagol/MainActivityTest.java
+++ app/src/test/java/org/fpezzato/yagol/MainActivityTest.java
@@ -7,11 +7,14 @@ import org.fpezzato.yagol.mvp.MvpState;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.util.ActivityController;
+
+import static org.mockito.MockitoAnnotations.initMocks;
 
 /**
  * Created by francesco on 17/06/2015.
@@ -21,61 +24,62 @@ import org.robolectric.util.ActivityController;
 @MediumTest
 public class MainActivityTest {
 
-	private MainActivity mMainActivity;
+	private MainActivity mActivitySubject;
 	private ActivityController<MainActivity> mMainActivityActivityController;
+
+	@Mock
+	MainActivityPresenter mPresenterMock;
+
+	@Mock
+	Bundle mBundleMock;
 
 	@Before
 	public void setUp() throws Exception {
+		initMocks(this);
+
 		mMainActivityActivityController = Robolectric.buildActivity(MainActivity.class);
-		mMainActivity = mMainActivityActivityController.get();
+		mActivitySubject = mMainActivityActivityController.get();
+
+		//inject the mock-presenter in the activity.
+		mActivitySubject.withPresenter(mPresenterMock);
 	}
 
 	@Test
 	public void mainActivity_initialize_presenter_TriggeredOnce() throws Exception {
-		MainActivityPresenter mockPresenter = Mockito.mock(MainActivityPresenter.class);
 
-		mMainActivity.withPresenter(mockPresenter);
-		Bundle bundleMock = Mockito.mock(Bundle.class);
 
-		mMainActivityActivityController.create(bundleMock);
+		//perform the activity lifecycle until onCreate
+		mMainActivityActivityController.create(mBundleMock);
 
-		Mockito.verify(mockPresenter, Mockito.times(1)).initialize(mMainActivity, new MvpState(bundleMock));
+		Mockito.verify(mPresenterMock, Mockito.times(1)).initialize(mActivitySubject, new MvpState(mBundleMock));
 	}
 
 	@Test
 	public void mainActivity_start_presenter_TriggeredOnce() throws Exception {
-		MainActivityPresenter mockPresenter = Mockito.mock(MainActivityPresenter.class);
 
-		mMainActivity.withPresenter(mockPresenter);
-		mMainActivityActivityController.create().start().visible();
+		//perform the activity lifecycle until onStart
+		mMainActivityActivityController.create().start();
 
-		Mockito.verify(mockPresenter, Mockito.times(1)).start();
+		Mockito.verify(mPresenterMock, Mockito.times(1)).start();
 
 	}
 
-
 	@Test
 	public void mainActivity_on_save_presenter_state_Triggered() throws Exception {
-		MainActivityPresenter mockPresenter = Mockito.mock(MainActivityPresenter.class);
 
-		mMainActivity.withPresenter(mockPresenter);
-		Bundle bundleMock = Mockito.mock(Bundle.class);
+		//perform the activity lifecycle until OnSaveInstanceState
+		mMainActivityActivityController.create().start().resume().pause().saveInstanceState(mBundleMock);
 
-		mMainActivityActivityController.create().start().resume().pause().saveInstanceState(bundleMock);
-
-		Mockito.verify(mockPresenter, Mockito.atLeastOnce()).onSaveInstanceState(new MvpState(bundleMock));
+		Mockito.verify(mPresenterMock, Mockito.atLeastOnce()).onSaveInstanceState(new MvpState(mBundleMock));
 	}
 
 	@Test
 	public void mainActivity_stop_presenter_Triggered() throws Exception {
-		MainActivityPresenter mockPresenter = Mockito.mock(MainActivityPresenter.class);
+		
+		//perform the activity lifecycle until stop
+		mMainActivityActivityController.create().start().resume().pause().saveInstanceState(mBundleMock).stop();
 
-		mMainActivity.withPresenter(mockPresenter);
-		Bundle bundleMock = Mockito.mock(Bundle.class);
-
-		mMainActivityActivityController.create().start().resume().pause().saveInstanceState(bundleMock).stop();
-
-		Mockito.verify(mockPresenter, Mockito.times(1)).stop();
+		Mockito.verify(mPresenterMock, Mockito.times(1)).stop();
 	}
 
 }
