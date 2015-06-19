029548798e0e8b05e49be194e4e2e99695fd2629
francesco pezzato <fpezzato@gmail.com>
2015-06-19 15:00:32 +0100

    Initial bit of unit test for MatrixView

---
+++ app/src/test/java/javax/microedition/khronos/opengles/GL.java
@@ -0,0 +1,5 @@
+package javax.microedition.khronos.opengles;
+
+//Workaround found in https://github.com/robolectric/robolectric-gradle-plugin/issues/145
+public interface GL {
+}
---
+++ app/src/test/java/org/fpezzato/yagol/MatrixViewTest.java
@@ -0,0 +1,48 @@
+package org.fpezzato.yagol;
+
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.test.suitebuilder.annotation.MediumTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by francesco on 19/06/2015.
+ */
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 19)
+@MediumTest
+public class MatrixViewTest {
+
+	MatrixView mMatrixView;
+
+	@Before
+	public void setUp() {
+		Activity activity = Robolectric.buildActivity(Activity.class).create().get();
+		mMatrixView = new MatrixView(activity);
+	}
+
+	@Test
+	public void matrixView_drawOnePoint_zoomLeve2() throws Exception {
+		Canvas canvasMock = Mockito.mock(Canvas.class);
+		Paint paintMock = Mockito.mock(Paint.class);
+
+		mMatrixView.setZoomLevel(2);
+		mMatrixView.drawZoomedPoint(canvasMock, paintMock, 0, 0);
+		Mockito.verify(canvasMock).drawPoint(0, 0, paintMock);
+		Mockito.verify(canvasMock).drawPoint(0, 1, paintMock);
+		Mockito.verify(canvasMock).drawPoint(1, 0, paintMock);
+		Mockito.verify(canvasMock).drawPoint(1, 1, paintMock);
+		Mockito.verify(canvasMock, Mockito.never()).drawPoint(2, 0, paintMock);
+		Mockito.verify(canvasMock, Mockito.never()).drawPoint(0, 2, paintMock);
+		Mockito.verify(canvasMock, Mockito.never()).drawPoint(2, 2, paintMock);
+	}
+}
